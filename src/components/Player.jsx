cdbef1760a13b619de6eaa801e2b96aded3908b5
Kevin Drew <kcdrew002@gmail.com>
2025-04-24 17:01:42 -0400

    Player input

---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+export default function Player({initialName, symbol}) {
+
+    const [playerName, setPlayerName] = useState({initialName});
+
+    const [isEditing, setIsEditing] =useState(false);
+    function handleEditClick(){
+        setIsEditing((editing) => !editing);
+
+    }
+
+    let editablePlayerName = <span className="player-name">{initialName}</span>
+
+    if (isEditing){
+        editablePlayerName = <input 
+                       type="text"
+                       required 
+                       value={initialName}
+                        />
+    }
+    return (
+        <>
+          <li>
+            <span className="player">
+                {editablePlayerName}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save': 'Edit'}</button>
+          </li>
+        
+        </>
+
+
+    )
+}
