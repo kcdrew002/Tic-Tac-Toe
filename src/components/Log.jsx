6e2c2b4a4bcb268c669be4ddd33cd2106e4e7090
Kevin Drew <kcdrew002@gmail.com>
2025-05-02 16:10:32 -0400

    Component clean up

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,8 +1,8 @@
-export function Log (){
+export function Log ({ turns }){
 
     return(
         <ol id="log">
-
+            {turns.map(turn => <li key= {`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row}, {turn.square.col}</li>)}
 
         </ol>
 
